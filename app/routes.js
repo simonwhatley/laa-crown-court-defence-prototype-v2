f38881eadd67893a24effed9e196f61589c58b1c
Simon Whatley <simonwhatley@gmail.com>
2019-07-14 21:07:24 +0100

    Add page stubs

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+router.get('/claim-summary', (req, res) => {
+  res.render('claim-summary');
+});
+
+router.get('/claim-details', (req, res) => {
+  res.render('claim-details');
+});
+
 // Add your routes here - above the module.exports line
 
 module.exports = router;
